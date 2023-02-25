e1686d869f409e45d9626288e94f0da2bb2b3cb5
Diya Mariam Biju <89064152+Diya-Mariam@users.noreply.github.com>
2023-02-25 22:38:09 +0530

    Add project files.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+class Game
+{
+    static void Main(String[] args)
+    {
+        string secretWord = "Diya M";
+        string guess = "";
+        int guessCount = 0;
+        bool outOfGuesses = false;
+
+        while(secretWord != guess && outOfGuesses== false)
+        {
+            if(guessCount < 3)
+            {
+                Console.WriteLine("Enter guess: ");
+                guess = Console.ReadLine();
+                guessCount++;
+            }
+            else
+            {
+                outOfGuesses = true;
+
+            }
+            
+        }
+
+        if( outOfGuesses)
+        {
+            Console.WriteLine("You Lost");
+        }
+        else
+        {
+            Console.WriteLine("You Won!");
+        }
+    }
+}
